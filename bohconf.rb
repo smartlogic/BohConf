ced2c823de7611dd236780d13c2f6cbda9a8d3d7
Nick Gauthier <nick@smartlogicsolutions.com>
2010-05-04 17:07:58 -0400

    send along to SLS, removed testing page

--- bohconf.rb
+++ bohconf.rb
@@ -2,7 +2,8 @@ require 'rubygems'
 require 'sinatra'
 
 get '/' do
-  redirect '/index.html'
+  redirect 'http://www.smartlogicsolutions.com'
+  #send_file 'public/index.html'
 end
 
 set :public, File.dirname(__FILE__) + "/public"
